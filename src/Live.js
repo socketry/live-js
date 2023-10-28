8c22bd6a7e6cd1c1455fa6127c4aad19f9d748a1
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-10-28 16:15:46 +1300

    Prefer 'module' style code.

---
+++ src/Live.js
@@ -0,0 +1,155 @@
+
+import morphdom from 'morphdom';
+
+export class Live {
+	constructor(window, url) {
+		this.window = window;
+		this.document = window.document;
+		
+		this.url = url;
+		this.events = [];
+		
+		this.failures = 0;
+		
+		// Track visibility state and connect if required:
+		this.document.addEventListener("visibilitychange", () => this.handleVisibilityChange());
+		this.handleVisibilityChange();
+	}
+	
+	connect() {
+		if (this.server) return this.server;
+		
+		let server = this.server = new this.window.WebSocket(this.url);
+		
+		server.onopen = () => {
+			this.failures = 0;
+			this.attach();
+		};
+		
+		server.onmessage = (message) => {
+			const [name, _arguments] = JSON.parse(message.data);
+			
+			this[name](..._arguments);
+		};
+		
+		// The remote end has disconnected:
+		server.addEventListener('error', () => {
+			this.failures += 1;
+		});
+		
+		server.addEventListener('close', () => {
+			// Explicit disconnect will clear `this.server`:
+			if (this.server) {
+				// We need a minimum delay otherwise this can end up immediately invoking the callback:
+				const delay = Math.max(100 * (this.failures + 1) ** 2, 60000);
+				setTimeout(() => this.connect(), delay);
+			}
+			
+			this.server = null;
+		});
+		
+		return server;
+	}
+	
+	disconnect() {
+		if (this.server) {
+			const server = this.server;
+			this.server = null;
+			server.close();
+		}
+	}
+	
+	createDocumentFragment(html) {
+		return this.document.createRange().createContextualFragment(html);
+	}
+	
+	// These methods are designed for RPC.
+	replace(id, html) {
+		let element = this.document.getElementById(id);
+		
+		morphdom(element, html);
+	}
+	
+	prepend(id, html) {
+		let element = this.document.getElementById(id);
+		let fragment = this.createDocumentFragment(html);
+		
+		element.prepend(fragment);
+	}
+	
+	append(id, html) {
+		let element = this.document.getElementById(id);
+		let fragment = this.createDocumentFragment(html);
+		
+		element.append(fragment);
+	}
+	
+	dispatch(id, type, details) {
+		let element = this.document.getElementById(id);
+		
+		element.dispatchEvent(
+			new CustomEvent(type, details)
+		);
+	}
+	
+	trigger(id, event) {
+		this.connect();
+		
+		this.send(
+			JSON.stringify({id: id, event: event})
+		);
+	}
+	
+	forward(id, event, details) {
+		this.trigger(id, {type: event.type, details: details});
+	}
+	
+	send(message) {
+		try {
+			this.server.send(message);
+		} catch (error) {
+			this.events.push(message);
+		}
+	}
+	
+	flush() {
+		if (this.events.length === 0) return;
+		
+		let events = this.events;
+		this.events = [];
+		
+		for (var event of events) {
+			this.send(event);
+		}
+	}
+	
+	bind(elements) {
+		for (var element of elements) {
+			this.send(JSON.stringify({bind: element.id, data: element.dataset}));
+		}
+	}
+	
+	bindElementsByClassName(selector = 'live') {
+		this.bind(
+			this.document.getElementsByClassName(selector)
+		);
+		
+		this.flush();
+	}
+	
+	handleVisibilityChange() {
+		if (this.document.hidden) {
+			this.disconnect();
+		} else {
+			this.connect();
+		}
+	}
+	
+	attach() {
+		if (this.document.readyState === 'loading') {
+			this.document.addEventListener('DOMContentLoaded', () => this.bindElementsByClassName());
+		} else {
+			this.bindElementsByClassName();
+		}
+	}
+}
---
+++ test/Live.js
@@ -0,0 +1,40 @@
+import {describe, before, after, it} from 'node:test';
+import {ok} from 'node:assert';
+
+import {WebSocket} from 'ws';
+import {JSDOM} from 'jsdom';
+import {Live} from '../src/Live.js';
+
+describe('Live', function () {
+	let dom;
+	let live;
+	let webSocketServer;
+
+	const webSocketServerConfig = {port: 3000};
+	const webSocketServerURL = `ws://localhost:${webSocketServerConfig.port}/live`;
+
+	before(async function () {
+		const listening = new Promise(resolve => {
+			webSocketServer = new WebSocket.Server(webSocketServerConfig, resolve);
+		});
+		
+		dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+		// Ensure the WebSocket class is available:
+		dom.window.WebSocket = WebSocket;
+		
+		await new Promise(resolve => dom.window.addEventListener('load', resolve));
+		await listening;
+		
+		live = new Live(dom.window, webSocketServerURL);
+	});
+
+	after(function () {
+		webSocketServer.close();
+	});
+
+	it('should connect to the WebSocket server', function () {
+		const server = live.connect();
+		ok(server);
+		live.disconnect();
+	});
+});
